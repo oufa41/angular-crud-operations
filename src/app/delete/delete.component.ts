ef41cfad1d8ce5650520beac8f45442f5a1bf613
ABD EL RAOUF Ahmed <ahmed.abd-el-raouf@cegedim.com>
2020-01-21 10:57:23 +0200

    delete updated

--- src/app/delete/delete.component.ts
+++ src/app/delete/delete.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder } from '@angular/forms';
+import { EmployeeService } from '../services/employee.service';
+
+
 @Component({
   selector: 'app-delete',
   templateUrl: './delete.component.html',
   styleUrls: ['./delete.component.css']
 })
 export class DeleteComponent implements OnInit {
-
-  constructor() { }
+  deleteEmployeeDataByIDForm;
+  constructor(
+    private usersService: EmployeeService,
+    private forBuilder: FormBuilder
+  ) { 
+    this.deleteEmployeeDataByIDForm = this.forBuilder.group({
+      employeeId: 0
+    });
+  }
 
   ngOnInit() {
   }
-
+  deleteEmployeeIdSubmit(employeeId: number) {
+    this.usersService.deleteEmployeeById(employeeId).subscribe(result => {
+      window.alert(result);
+    },error => window.alert(error))
+  }
 }
+
